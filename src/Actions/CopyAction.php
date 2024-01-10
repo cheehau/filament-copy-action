204c622c4855e5f3df18a327c1d801b0d0985d7b
Chee Hau <cheehau.lee11@gmail.com>
2024-01-10 13:47:47 +0800

    Update CopyAction.php

--- src/Actions/CopyAction.php
+++ src/Actions/CopyAction.php
@@ -18,13 +18,12 @@ class CopyAction extends BaseAction
         parent::setUp();
 
         $this
-            ->successNotificationTitle(__('Copied!'))
             ->icon('heroicon-o-clipboard-document')
             ->livewireClickHandlerEnabled(false)
             ->extraAttributes(fn () => [
                 'x-on:click' => new HtmlString(
                     'window.navigator.clipboard.writeText('.Js::from($this->getContent()).');'
-                    .(($title = $this->getSuccessNotificationTitle()) ? ' $tooltip('.Js::from($title).');' : '')
+                    .(($message = $this->getCopyMessage()) ? ' $tooltip('.Js::from($message).');' : '')
                 ),
             ]);
     }
